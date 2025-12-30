1f2b4b2704537620fd5fb4e4d5f6d1d244e95aac
adityahalge <adityahalge127@gmail.com>
2025-12-30 21:51:57 +0530

    Update command_mapper.py

--- command_mapper.py
+++ command_mapper.py
@@ -4,35 +4,39 @@ NODEMCU_IP = "192.168.1.50"
 BASE_URL = f"http://{NODEMCU_IP}"
 
 IOT_COMMANDS = {
-    "RED_ON": "/red_on",
-    "RED_OFF": "/red_off",
-    "YELLOW_ON": "/yellow_on",
-    "YELLOW_OFF": "/yellow_off",
+    "red_on": "/red_on",
+    "red_off": "/red_off",
+    "yellow_on": "/yellow_on",
+    "yellow_off": "/yellow_off",
+    "green_on" : "/green_on"
+    "green_off" : "/green_off"
     "BOTH_ON": "/both_on",
     "BOTH_OFF": "/both_off",
 }
 
 COMMUNICATION = {
     
-    "HELLO": "Hello",
-    "YES": "Yes",
-    "NO": "No",
-    "THANK_YOU": "Thank you",
-    "HELP": "Help"
-
+    "hello": "Hello",
+    "yes": "Yes",
+    "no": "No",
+    "help": "Help",
+    "food" : "Food",
+    "mission_passed" : "Mission Passed"
 
 }
 
 def send_command(endpoint):
-    print(f"[IoT] Sending request to {BASE_URL}{endpoint}")
-    # requests.get(BASE_URL + endpoint)
+    try:
+        requests.get(BASE_URL + endpoint, timeout=2)
+    except:
+        print("NodeMCU not reachable")
 
 def process_gesture(gesture):
+    gesture = gesture.strip().lower()
+
     if gesture in IOT_COMMANDS:
         send_command(IOT_COMMANDS[gesture])
     elif gesture in COMMUNICATION:
-        print(f"[COMMUNICATION] {gesture}")
-    elif gesture == "CUSTOM_ACTION":
-        print("[CUSTOM] Custom gesture detected")
+        print(COMMUNICATION[gesture])
     else:
-        print("[UNKNOWN] Gesture")
+        print("Unknown gesture:", gesture)
